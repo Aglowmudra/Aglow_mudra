4e678996bc4d7147d8967ff2b111b7958a0475bc
Drfin <vipinj444@gmail.com>
2020-01-06 12:06:54 +0530

    Master_update

--- app/src/main/java/com/example/aglowmudra/Login.java
+++ app/src/main/java/com/example/aglowmudra/Login.java
@@ -1,14 +1,46 @@
 package com.example.aglowmudra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
-
+    public static final int REQUEST_READ_CONTACTS = 79;
+    public static final int REQUEST_READ_STORAGE= 79;
+    ListView list;
+    ArrayList mobileArray;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)==PackageManager.PERMISSION_GRANTED){
+
+        }else{
+requestPermission();
+        }
+
+    }
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+            // show UI part if you want here to show some rationale !!!
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        }
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        }
+
     }
 }
