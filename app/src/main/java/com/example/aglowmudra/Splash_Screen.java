271bf8bba2e038f7c123b03e3433f1ac7b3743b8
Drfin <vipinj444@gmail.com>
2020-01-06 11:05:03 +0530

    Master_update

--- app/src/main/java/com/example/aglowmudra/Splash_Screen.java
+++ app/src/main/java/com/example/aglowmudra/Splash_Screen.java
@@ -2,7 +2,9 @@ package com.example.aglowmudra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+        try{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent=new Intent(Splash_Screen.this,Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },1000);
+
+        }catch (Exception e){
+            e.getStackTrace();
+        }
     }
 }
